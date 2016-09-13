5384371b19052981b0c5708df5d30db290cd2201
vdhan <anvudh@gmail.com>
2016-09-13 17:58:25 +0700

    fix miscellaneous

--- An.py
+++ An.py
@@ -3,190 +3,210 @@ An's Python library
 Author: Hoàng Ân
 Copyright © All rights reserved
 """
-import math, fractions, hashlib
+import hashlib
+import math
 
-def isPrime(n = 0):
-	"""Check whether a number is prime"""
-	if n < 2 or math.isfinite(n) == False:
-		return False
 
-	if n % 2 == 0:
-		return False if n != 2 else True
+def is_prime(n=0):
+    """Check whether a number is prime"""
+    if n < 2 or not math.isfinite(n):
+        return False
 
-	if n % 3 == 0:
-		return False if n != 3 else True
-		
-	if n % 5 == 0:
-		return False if n != 5 else True
+    if n % 2 == 0:
+        return False if n != 2 else True
 
-	m = math.floor(n ** 0.5)
-	for i in range(7, m + 1, 2):
-		if n % i == 0:
-			return False
+    if n % 3 == 0:
+        return False if n != 3 else True
 
-	return True
+    if n % 5 == 0:
+        return False if n != 5 else True
+
+    m = math.floor(n ** 0.5)
+    for i in range(7, m + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+
 
 def trim(m):
-	"""Trim string"""
-	l = m.split()
-	return ' '.join(l)
+    """Trim string"""
+    l = m.split()
+    return ' '.join(l)
+
 
 def gcd(*a):
-	"""Return the greatest common divisor for 2 or more numbers"""
-	if len(a) < 2:
-		raise TypeError('gcd() takes at least 2 arguments')
+    """Return the greatest common divisor for 2 or more numbers"""
+    if len(a) < 2:
+        raise TypeError('gcd() takes at least 2 arguments')
 
-	for i in a:
-		if math.isfinite(i) == False:
-			raise TypeError('Parameter Error!')
+    for i in a:
+        if not math.isfinite(i):
+            raise TypeError('Parameter Error!')
 
-	b = fractions.gcd(a[0], a[1])
-	for i in range(2, len(a)):
-		b = fractions.gcd(b, a[i])
+    b = math.gcd(a[0], a[1])
+    for i in range(2, len(a)):
+        b = math.gcd(b, a[i])
 
-	return b
+    return b
+
 
 def lcm(*a):
-	"""Return the least common multiple for 2 or more numbers"""
-	if len(a) < 2:
-		raise TypeError('gcd() takes at least 2 arguments')
+    """Return the least common multiple for 2 or more numbers"""
+    if len(a) < 2:
+        raise TypeError('gcd() takes at least 2 arguments')
 
-	for i in a:
-		if math.isfinite(i) == False:
-			raise TypeError('Parameter Error!')
+    for i in a:
+        if not math.isfinite(i):
+            raise TypeError('Parameter Error!')
 
-	b = bcnn(a[0], a[1])
-	for i in range(2, len(a)):
-		b = bcnn(b, a[i])
+    b = bcnn(a[0], a[1])
+    for i in range(2, len(a)):
+        b = bcnn(b, a[i])
 
-	return b
+    return b
+
 
 def bcnn(a, b):
-	"""Return the least common multiple for 2 numbers"""
-	if math.isfinite(a) == False or math.isfinite(b) == False:
-		raise TypeError('Parameter Error!')
+    """Return the least common multiple for 2 numbers"""
+    if not math.isfinite(a) or not math.isfinite(b):
+        raise TypeError('Parameter Error!')
 
-	return a * b / fractions.gcd(a, b)
-	
-def isFinite(n):
-	"""Check if finite number"""
-	try:
-		a = float(n)
-		return math.isfinite(a)
-	except:
-		return False
-		
-def isNum(n):
-	"""Check if number (include infinity)"""
-	try:
-		a = float(n)
-		return not math.isnan(a)
-	except:
-		return False
-	
-def isInt(n):
-	"""Check if integer"""
-	try:
-		return n[1:].isdigit() if n[0] in ('-', '+') else n.isdigit()
-	except:
-		pass
-	
-	try:
-		return int(n) == n
-	except:
-		return False
+    return a * b / math.gcd(a, b)
 
-def inverse(a, z = 26):
-	"""Return the modular multiplicative inverse of given number"""
-	if math.isfinite(a) == False or math.isfinite(z) == False:
-		raise TypeError('Parameter Error!')
-		
-	a = int(a)
-	z = int(z)
-	if a < 0:
-		a %= z
-		
-	if gcd(a, z) == 1:
-		n, y, y2 = z, 1, 0
-		while a != 0:
-			q = z // a
-			z, a = a, z % a
-			y, y2 = y2 - q * y, y
-		return y2 % n
-		
-	return 0
+
+def is_finite(n):
+    """Check if finite number"""
+    try:
+        a = float(n)
+        return math.isfinite(a)
+    except:
+        return False
+
+
+def is_num(n):
+    """Check if number (include infinity)"""
+    try:
+        a = float(n)
+        return not math.isnan(a)
+    except:
+        return False
+
+
+def is_int(n):
+    """Check if integer"""
+    try:
+        return n[1:].isdigit() if n[0] in ('-', '+') else n.isdigit()
+    except:
+        pass
+
+    try:
+        return int(n) == n
+    except:
+        return False
+
+
+def inverse(a, z=26):
+    """Return the modular multiplicative inverse of given number"""
+    if not math.isfinite(a) or not math.isfinite(z):
+        raise TypeError('Parameter Error!')
+
+    a = int(a)
+    z = int(z)
+    if a < 0:
+        a %= z
+
+    if gcd(a, z) == 1:
+        n, y, y2 = z, 1, 0
+        while a != 0:
+            q = z // a
+            z, a = a, z % a
+            y, y2 = y2 - q * y, y
+        return y2 % n
+
+    return 0
+
 
 def base36encode(n):
-	"""Convert integer to hexatridecimal"""
-	if math.isfinite(n) == False:
-		raise TypeError('Parameter Error!')
-		
-	n = int(n)
-	alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	base = ''
-	sign = ''
-	
-	if n < 0:
-		sign = '-'
-		n = -n
-		
-	if n in range(36):
-		return sign + alpha[n]
-	
-	while n != 0:
-		n, i = n // 36, n % 36
-		base = alpha[i] + base
-		
-	return sign + base
+    """Convert integer to hexatridecimal"""
+    if not math.isfinite(n):
+        raise TypeError('Parameter Error!')
 
-def encrypt(m, algorithm = 'sha512'):
-	"""Hash function for given cryptography (default SHA-512)"""
-	h = hashlib.new(algorithm)
-	h.update(m.encode('utf-8'))
-	return h.hexdigest()
-	
+    n = int(n)
+    alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    base = ''
+    sign = ''
+
+    if n < 0:
+        sign = '-'
+        n = -n
+
+    if n in range(36):
+        return sign + alpha[n]
+
+    while n != 0:
+        n, i = n // 36, n % 36
+        base = alpha[i] + base
+
+    return sign + base
+
+
+def encrypt(m, algorithm='sha512'):
+    """Hash function for given cryptography (default SHA-512)"""
+    h = hashlib.new(algorithm)
+    h.update(m.encode('utf-8'))
+    return h.hexdigest()
+
+
 def permutation(n, r):
-	"""Return the number of r-permutations of a set with n elements"""
-	return math.factorial(n) // math.factorial(n - r)
+    """Return the number of r-permutations of a set with n elements"""
+    return math.factorial(n) // math.factorial(n - r)
+
 
 def combination(n, r):
-	"""Return the number of r-combinations of a set with n elements"""
-	return math.factorial(n) // math.factorial(n - r) // math.factorial(r)
+    """Return the number of r-combinations of a set with n elements"""
+    return math.factorial(n) // math.factorial(n - r) // math.factorial(r)
+
 
 def rounding(x):
-	"""Return the rounding of x"""
-	t = abs(x - int(x))
-	if x < 0:
-		x = int(x - 1) if t > 0.5 else int(x)
-	else:
-		x = int(x) if t < 0.5 else int(x + 1)
-	return x
+    """Return the rounding of x"""
+    t = abs(x - int(x))
+    if x < 0:
+        x = int(x - 1) if t > 0.5 else int(x)
+    else:
+        x = int(x) if t < 0.5 else int(x + 1)
+    return x
+
 
 def bfs(g, s, d):
-    queue = []
-    queue.append([s])
-    
+    queue = [[s]]
+
     while queue:
         path = queue.pop(0)
         node = path[-1]
         if node == d:
             return path
-        
+
         for adjacent in g.get(node, []):
             new_path = list(path)
             new_path.append(adjacent)
             queue.append(new_path)
 
-def dfs(g, s, d, p=[]):
+
+def dfs(g, s, d, p=None):
+    if p is None:
+        p = []
+
     p = p + [s]
     if s == d:
         return p
-    
+
     for node in g[s]:
         if node not in p:
             newpath = dfs(g, node, d, p)
             if newpath:
                 return newpath
 
+
 if __name__ == '__main__':
-	print("An's Python library\nAuthor: Hoàng Ân\nCopyright © All rights reserved")
+    print("An's Python library\nAuthor: Hoàng Ân\nCopyright © All rights reserved")
--- __main__.py
+++ __main__.py
@@ -1,313 +1,317 @@
 import os
-path = os.path.dirname(__file__) + '/'
-
 from tkinter import *
 from tkinter.ttk import *
 from An import *
 
+path = os.path.dirname(__file__) + '/'
+
+
 def decrypt(m, n):
-	s = ''
-	
-	for c in m:
-		i = ord(c)
-		
-		if i in range(97, 123):
-			i -= 97
-			i = (i - n) % 26
-			i += 97
-			s += chr(i)
-			
-		elif i in range(65, 91):
-			i -= 65
-			i = (i - n) % 26
-			i += 65
-			s += chr(i)
-			
-		else:
-			s += c
-	
-	return s
+    s = ''
+
+    for c in m:
+        i = ord(c)
+
+        if i in range(97, 123):
+            i -= 97
+            i = (i - n) % 26
+            i += 97
+            s += chr(i)
+
+        elif i in range(65, 91):
+            i -= 65
+            i = (i - n) % 26
+            i += 65
+            s += chr(i)
+
+        else:
+            s += c
+
+    return s
+
 
 def cryptanalysis():
-	a = 0
-	e = 4
-	i = 8
-	o = 14
-	t = 19
-	g = False
-	
-	pt1Str.set('')
-	n1Str.set('')
-	
-	pt2Str.set('')
-	n2Str.set('')
-	
-	pt3Str.set('')
-	n3Str.set('')
-	
-	pt4Str.set('')
-	n4Str.set('')
-	
-	pt5Str.set('')
-	n5Str.set('')
-	
-	pt6Str.set('')
-	n6Str.set('')
-	
-	pt7Str.set('')
-	n7Str.set('')
-	
-	pt8Str.set('')
-	n8Str.set('')
-	
-	pt9Str.set('')
-	n9Str.set('')
+    na = 0
+    ni = 0
+    e = 4
+    i = 8
+    o = 14
+    t = 19
+    g = False
 
-	pt0Str.set('')
-	n0Str.set('')
-	
-	m = mText.get(1.0, 'end')
-	m = trim(m)
-	
-	l = m.split()
-	
-	for c in l:
-		if len(c) == 1:
-			b = ord(c)
-			
-			if b in range(65, 91):
-				b -= 65
-				na = b
-				ni = b - i
-				g = True
-				break
-			
-			elif b in range(97, 123):
-				b -= 97
-				na = b
-				ni = b - i
-				g = True
-				break
-	
-	if g == True and m != '':
-		s = decrypt(m, na)
-		pt1Str.set(s)
-		n1Str.set(na)
-		
-		s = decrypt(m, ni)
-		pt2Str.set(s)
-		n2Str.set(ni)
-		
-	elif g == False and m != '':
-		d = [0] * 26
-		l = m.lower()
-		
-		for c in l:
-			b = ord(c)
-			if b in range(97, 123):
-				d[b - 97] += 1
-		
-		b = max(d)
-		x = d.index(b)
-		d.remove(b)
-		
-		b = max(d)
-		y = d.index(b)
-		l = []
-		
-		na0 = x
-		ne0 = x - e
-		ni0 = x - i
-		no0 = x - o
-		nt0 = x - t
-		
-		na1 = y
-		ne1 = y - e
-		ni1 = y - i
-		no1 = y - o
-		nt1 = y - t
-		
-		s = decrypt(m, na0)
-		pt1Str.set(s)
-		n1Str.set(na0)
-		l.append(na0)
-		
-		if ne0 not in l:
-			s = decrypt(m, ne0)
-			pt2Str.set(s)
-			n2Str.set(ne0)
-			l.append(ne0)
-		
-		if ni0 not in l:
-			s = decrypt(m, ni0)
-			pt3Str.set(s)
-			n3Str.set(ni0)
-			l.append(ni0)
-		
-		if no0 not in l:
-			s = decrypt(m, no0)
-			pt4Str.set(s)
-			n4Str.set(no0)
-			l.append(no0)
-		
-		if nt0 not in l:
-			s = decrypt(m, nt0)
-			pt5Str.set(s)
-			n5Str.set(nt0)
-			l.append(nt0)
-		
-		if na1 not in l:
-			s = decrypt(m, na1)
-			pt6Str.set(s)
-			n6Str.set(na1)
-			l.append(na1)
-		
-		if ne1 not in l:
-			s = decrypt(m, ne1)
-			pt7Str.set(s)
-			n7Str.set(ne1)
-			l.append(ne1)
-		
-		if ni1 not in l:
-			s = decrypt(m, ni1)
-			pt8Str.set(s)
-			n8Str.set(ni1)
-			l.append(ni1)
-		
-		if no1 not in l:
-			s = decrypt(m, no1)
-			pt9Str.set(s)
-			n9Str.set(no1)
-			l.append(no1)
-		
-		if nt1 not in l:
-			s = decrypt(m, nt1)
-			pt0Str.set(s)
-			n0Str.set(nt1)
-			l.append(nt1)
+    pt1Str.set('')
+    n1Str.set('')
+
+    pt2Str.set('')
+    n2Str.set('')
+
+    pt3Str.set('')
+    n3Str.set('')
+
+    pt4Str.set('')
+    n4Str.set('')
+
+    pt5Str.set('')
+    n5Str.set('')
+
+    pt6Str.set('')
+    n6Str.set('')
+
+    pt7Str.set('')
+    n7Str.set('')
+
+    pt8Str.set('')
+    n8Str.set('')
+
+    pt9Str.set('')
+    n9Str.set('')
+
+    pt0Str.set('')
+    n0Str.set('')
+
+    m = mText.get(1.0, 'end')
+    m = trim(m)
+
+    l = m.split()
+
+    for c in l:
+        if len(c) == 1:
+            b = ord(c)
+
+            if b in range(65, 91):
+                b -= 65
+                na = b
+                ni = b - i
+                g = True
+                break
+
+            elif b in range(97, 123):
+                b -= 97
+                na = b
+                ni = b - i
+                g = True
+                break
+
+    if g and m != '':
+        s = decrypt(m, na)
+        pt1Str.set(s)
+        n1Str.set(na)
+
+        s = decrypt(m, ni)
+        pt2Str.set(s)
+        n2Str.set(ni)
+
+    elif not g and m != '':
+        d = [0] * 26
+        l = m.lower()
+
+        for c in l:
+            b = ord(c)
+            if b in range(97, 123):
+                d[b - 97] += 1
+
+        b = max(d)
+        x = d.index(b)
+        d.remove(b)
+
+        b = max(d)
+        y = d.index(b)
+        l = []
+
+        na0 = x
+        ne0 = x - e
+        ni0 = x - i
+        no0 = x - o
+        nt0 = x - t
+
+        na1 = y
+        ne1 = y - e
+        ni1 = y - i
+        no1 = y - o
+        nt1 = y - t
+
+        s = decrypt(m, na0)
+        pt1Str.set(s)
+        n1Str.set(na0)
+        l.append(na0)
+
+        if ne0 not in l:
+            s = decrypt(m, ne0)
+            pt2Str.set(s)
+            n2Str.set(ne0)
+            l.append(ne0)
+
+        if ni0 not in l:
+            s = decrypt(m, ni0)
+            pt3Str.set(s)
+            n3Str.set(ni0)
+            l.append(ni0)
+
+        if no0 not in l:
+            s = decrypt(m, no0)
+            pt4Str.set(s)
+            n4Str.set(no0)
+            l.append(no0)
+
+        if nt0 not in l:
+            s = decrypt(m, nt0)
+            pt5Str.set(s)
+            n5Str.set(nt0)
+            l.append(nt0)
+
+        if na1 not in l:
+            s = decrypt(m, na1)
+            pt6Str.set(s)
+            n6Str.set(na1)
+            l.append(na1)
+
+        if ne1 not in l:
+            s = decrypt(m, ne1)
+            pt7Str.set(s)
+            n7Str.set(ne1)
+            l.append(ne1)
+
+        if ni1 not in l:
+            s = decrypt(m, ni1)
+            pt8Str.set(s)
+            n8Str.set(ni1)
+            l.append(ni1)
+
+        if no1 not in l:
+            s = decrypt(m, no1)
+            pt9Str.set(s)
+            n9Str.set(no1)
+            l.append(no1)
+
+        if nt1 not in l:
+            s = decrypt(m, nt1)
+            pt0Str.set(s)
+            n0Str.set(nt1)
+            l.append(nt1)
+
 
 if __name__ == '__main__':
-	root = Tk()
-	root.title("An's shift cipher cryptanalysis v1.0")
-	img = PhotoImage(file = path + 'icon.gif')
-	root.tk.call('wm', 'iconphoto', root._w, img)
-	root.resizable(False, False)
-	
-	style = Style()
-	themes = style.theme_names()
+    root = Tk()
+    root.title("An's shift cipher cryptanalysis v1.0")
+    img = PhotoImage(file=path + 'icon.gif')
+    root.tk.call('wm', 'iconphoto', root._w, img)
+    root.resizable(False, False)
 
-	if 'xpnative' in themes:
-		style.theme_use('xpnative')
-	elif 'aqua' in themes:
-		style.theme_use('aqua')
-	elif 'alt' in themes:
-		style.theme_use('alt')
-	else:
-		style.theme_use('default')
-		
-	mf = Frame(root, padding = 10)
-	mf.grid(column = 0, row = 0, sticky = 'wnes')
-	
-	inLf = Labelframe(mf, text = 'Nhập bản mã Shift')
-	inLf.grid(column = 0, row = 0, ipady = 10, padx = 5)
-	
-	inLbl = Label(inLf, text = 'Bản mã:')
-	inLbl.grid(column = 0, row = 0)
-	
-	mText = Text(inLf, width = 40, height = 9)
-	mText.grid(column = 0, row = 1, padx = 10, pady = 10)
-	
-	cryptBtn = Button(inLf, text = 'Thám mã', command = cryptanalysis)
-	cryptBtn.grid(column = 0, row = 2)
-	
-	outLf = Labelframe(mf, text = '10 kết quả khả nghi')
-	outLf.grid(column = 1, row = 0, ipady = 10, ipadx = 10, padx = 5, sticky = 'n')
-	
-	ptLbl = Label(outLf, text = 'Bản rõ:')
-	ptLbl.grid(column = 0, row = 0)
-	
-	nLbl = Label(outLf, text = 'n')
-	nLbl.grid(column = 1, row = 0)
-	
-	pt1Str = StringVar()
-	pt1Lbl = Label(outLf, textvariable = pt1Str)
-	pt1Lbl.grid(row = 1, column = 0)
-	
-	n1Str = StringVar()
-	n1Lbl = Label(outLf, textvariable = n1Str)
-	n1Lbl.grid(row = 1, column = 1)
-	
-	pt2Str = StringVar()
-	pt2Lbl = Label(outLf, textvariable = pt2Str)
-	pt2Lbl.grid(row = 2, column = 0)
-	
-	n2Str = StringVar()
-	n2Lbl = Label(outLf, textvariable = n2Str)
-	n2Lbl.grid(row = 2, column = 1)
-	
-	pt3Str = StringVar()
-	pt3Lbl = Label(outLf, textvariable = pt3Str)
-	pt3Lbl.grid(row = 3, column = 0)
-	
-	n3Str = StringVar()
-	n3Lbl = Label(outLf, textvariable = n3Str)
-	n3Lbl.grid(row = 3, column = 1)
-	
-	pt4Str = StringVar()
-	pt4Lbl = Label(outLf, textvariable = pt4Str)
-	pt4Lbl.grid(row = 4, column = 0)
-	
-	n4Str = StringVar()
-	n4Lbl = Label(outLf, textvariable = n4Str)
-	n4Lbl.grid(row = 4, column = 1)
-	
-	pt5Str = StringVar()
-	pt5Lbl = Label(outLf, textvariable = pt5Str)
-	pt5Lbl.grid(row = 5, column = 0)
-	
-	n5Str = StringVar()
-	n5Lbl = Label(outLf, textvariable = n5Str)
-	n5Lbl.grid(row = 5, column = 1)
-	
-	pt6Str = StringVar()
-	pt6Lbl = Label(outLf, textvariable = pt6Str)
-	pt6Lbl.grid(row = 6, column = 0)
-	
-	n6Str = StringVar()
-	n6Lbl = Label(outLf, textvariable = n6Str)
-	n6Lbl.grid(row = 6, column = 1)
-	
-	pt7Str = StringVar()
-	pt7Lbl = Label(outLf, textvariable = pt7Str)
-	pt7Lbl.grid(row = 7, column = 0)
-	
-	n7Str = StringVar()
-	n7Lbl = Label(outLf, textvariable = n7Str)
-	n7Lbl.grid(row = 7, column = 1)
-	
-	pt8Str = StringVar()
-	pt8Lbl = Label(outLf, textvariable = pt8Str)
-	pt8Lbl.grid(row = 8, column = 0)
-	
-	n8Str = StringVar()
-	n8Lbl = Label(outLf, textvariable = n8Str)
-	n8Lbl.grid(row = 8, column = 1)
-	
-	pt9Str = StringVar()
-	pt9Lbl = Label(outLf, textvariable = pt9Str)
-	pt9Lbl.grid(row = 9, column = 0)
-	
-	n9Str = StringVar()
-	n9Lbl = Label(outLf, textvariable = n9Str)
-	n9Lbl.grid(row = 9, column = 1)
-	
-	pt0Str = StringVar()
-	pt0Lbl = Label(outLf, textvariable = pt0Str)
-	pt0Lbl.grid(row = 10, column = 0)
-	
-	n0Str = StringVar()
-	n0Lbl = Label(outLf, textvariable = n0Str)
-	n0Lbl.grid(row = 10, column = 1)
-	
-	mText.focus()
-	root.mainloop()
+    style = Style()
+    themes = style.theme_names()
+
+    if 'xpnative' in themes:
+        style.theme_use('xpnative')
+    elif 'aqua' in themes:
+        style.theme_use('aqua')
+    elif 'alt' in themes:
+        style.theme_use('alt')
+    else:
+        style.theme_use('default')
+
+    mf = Frame(root, padding=10)
+    mf.grid(column=0, row=0, sticky='wnes')
+
+    inLf = Labelframe(mf, text='Nhập bản mã Shift')
+    inLf.grid(column=0, row=0, ipady=10, padx=5)
+
+    inLbl = Label(inLf, text='Bản mã:')
+    inLbl.grid(column=0, row=0)
+
+    mText = Text(inLf, width=40, height=9)
+    mText.grid(column=0, row=1, padx=10, pady=10)
+
+    cryptBtn = Button(inLf, text='Thám mã', command=cryptanalysis)
+    cryptBtn.grid(column=0, row=2)
+
+    outLf = Labelframe(mf, text='10 kết quả khả nghi')
+    outLf.grid(column=1, row=0, ipady=10, ipadx=10, padx=5, sticky='n')
+
+    ptLbl = Label(outLf, text='Bản rõ:')
+    ptLbl.grid(column=0, row=0)
+
+    nLbl = Label(outLf, text='n')
+    nLbl.grid(column=1, row=0)
+
+    pt1Str = StringVar()
+    pt1Lbl = Label(outLf, textvariable=pt1Str)
+    pt1Lbl.grid(row=1, column=0)
+
+    n1Str = StringVar()
+    n1Lbl = Label(outLf, textvariable=n1Str)
+    n1Lbl.grid(row=1, column=1)
+
+    pt2Str = StringVar()
+    pt2Lbl = Label(outLf, textvariable=pt2Str)
+    pt2Lbl.grid(row=2, column=0)
+
+    n2Str = StringVar()
+    n2Lbl = Label(outLf, textvariable=n2Str)
+    n2Lbl.grid(row=2, column=1)
+
+    pt3Str = StringVar()
+    pt3Lbl = Label(outLf, textvariable=pt3Str)
+    pt3Lbl.grid(row=3, column=0)
+
+    n3Str = StringVar()
+    n3Lbl = Label(outLf, textvariable=n3Str)
+    n3Lbl.grid(row=3, column=1)
+
+    pt4Str = StringVar()
+    pt4Lbl = Label(outLf, textvariable=pt4Str)
+    pt4Lbl.grid(row=4, column=0)
+
+    n4Str = StringVar()
+    n4Lbl = Label(outLf, textvariable=n4Str)
+    n4Lbl.grid(row=4, column=1)
+
+    pt5Str = StringVar()
+    pt5Lbl = Label(outLf, textvariable=pt5Str)
+    pt5Lbl.grid(row=5, column=0)
+
+    n5Str = StringVar()
+    n5Lbl = Label(outLf, textvariable=n5Str)
+    n5Lbl.grid(row=5, column=1)
+
+    pt6Str = StringVar()
+    pt6Lbl = Label(outLf, textvariable=pt6Str)
+    pt6Lbl.grid(row=6, column=0)
+
+    n6Str = StringVar()
+    n6Lbl = Label(outLf, textvariable=n6Str)
+    n6Lbl.grid(row=6, column=1)
+
+    pt7Str = StringVar()
+    pt7Lbl = Label(outLf, textvariable=pt7Str)
+    pt7Lbl.grid(row=7, column=0)
+
+    n7Str = StringVar()
+    n7Lbl = Label(outLf, textvariable=n7Str)
+    n7Lbl.grid(row=7, column=1)
+
+    pt8Str = StringVar()
+    pt8Lbl = Label(outLf, textvariable=pt8Str)
+    pt8Lbl.grid(row=8, column=0)
+
+    n8Str = StringVar()
+    n8Lbl = Label(outLf, textvariable=n8Str)
+    n8Lbl.grid(row=8, column=1)
+
+    pt9Str = StringVar()
+    pt9Lbl = Label(outLf, textvariable=pt9Str)
+    pt9Lbl.grid(row=9, column=0)
+
+    n9Str = StringVar()
+    n9Lbl = Label(outLf, textvariable=n9Str)
+    n9Lbl.grid(row=9, column=1)
+
+    pt0Str = StringVar()
+    pt0Lbl = Label(outLf, textvariable=pt0Str)
+    pt0Lbl.grid(row=10, column=0)
+
+    n0Str = StringVar()
+    n0Lbl = Label(outLf, textvariable=n0Str)
+    n0Lbl.grid(row=10, column=1)
+
+    mText.focus()
+    root.mainloop()
